8c06b94fe86531f9ae233edb32f6415887e09010
richard <richard@localhost>
2023-10-05 10:44:01 -0500

    Done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,21 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setXscale(0, 22);
+		StdDraw.setYscale(0, 16);
 		
+		StdDraw.clear();
+		
+		StdDraw.setPenColor(62, 102, 173);
+		StdDraw.filledRectangle(3, 3, 3, 3);
+		StdDraw.filledRectangle(3, 13, 3, 3);
+		StdDraw.filledRectangle(16, 3, 6, 3);
+		StdDraw.filledRectangle(16, 13, 6, 3);
+		StdDraw.setPenColor(255, 255, 255);
+		StdDraw.filledCircle(8, 8, 5);
+		StdDraw.setPenColor(0, 0, 127);
+		StdDraw.filledRectangle(11, 8, 11, 1);
+		StdDraw.filledRectangle(8, 8, 1, 8);
+		StdDraw.filledCircle(8, 8, 3);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,6 +1,7 @@
 package studio4;
 
 import java.io.File;
+
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
@@ -19,6 +20,71 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		String type = in.next();
+		
+		if(type.equals("rectangle")) {
+			int redComponent = in.nextInt();
+			int greenComponent = in.nextInt();
+			int blueComponent = in.nextInt();
+			boolean isFilled = in.nextBoolean();
+			double p1 = in.nextDouble();
+			double p2 = in.nextDouble();
+			double p3 = in.nextDouble();
+			double p4 = in.nextDouble();
+			
+			StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+			
+			if(isFilled) {
+				StdDraw.filledRectangle(p1, p2, p3, p4);
+			}else {
+				StdDraw.rectangle(p1, p2, p3, p4);
+			}
+			
+			
+		}else if(type.equals("ellipse")) {
+			int redComponent = in.nextInt();
+			int greenComponent = in.nextInt();
+			int blueComponent = in.nextInt();
+			boolean isFilled = in.nextBoolean();
+			double p1 = in.nextDouble();
+			double p2 = in.nextDouble();
+			double p3 = in.nextDouble();
+			double p4 = in.nextDouble();
+			
+			StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+			
+			if(isFilled) {
+				StdDraw.filledEllipse(p1, p2, p3, p4);
+			}else {
+				StdDraw.ellipse(p1, p2, p3, p4);
+			}
+			
+			
+		}else {
+			int redComponent = in.nextInt();
+			int greenComponent = in.nextInt();
+			int blueComponent = in.nextInt();
+			boolean isFilled = in.nextBoolean();
+			double p1 = in.nextDouble();
+			double p2 = in.nextDouble();
+			double p3 = in.nextDouble();
+			double p4 = in.nextDouble();
+			double p5 = in.nextDouble();
+			double p6 = in.nextDouble();
+			
+			double[] x = new double[] {p1,p3,p5};
+			double[] y = new double[] {p2,p4,p6};
+			
+			StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+			
+			if(isFilled) {
+				StdDraw.filledPolygon(x,y);
+			}else {
+				StdDraw.polygon(x, y);
+			}
+			
+		}
 		
 	}
 }
